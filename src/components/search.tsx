a7890e8fc5c9f055a534f58685a45667bd581673
bxantus <drx.trash@gmail.com>
2020-04-20 17:44:20 +0200

    Started working on real search

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { RadioSearch } from '../functions/radioSearch';
 
-export default class Search extends React.Component {
+interface SearchState {
+    search: RadioSearch|undefined
+}
+
+export default class Search extends React.Component<{}, SearchState> {
+    constructor(props) {
+        super(props)
+        this.state = {
+            search: undefined
+        }
+    }
+
+    async searchTextChanged(e) {
+        const query = e.target.value;
+        console.log("Search: ", query)
+        
+    }
+
+    private searchTimer
+    async scheduleSearch(query:string) {
+        this.searchTimer = setInterval(async ()=> {
+            let search = new RadioSearch({name: query})
+            await search.search()
+            this.setState({
+                search
+            })
+        })
+    }
+
     render() {
         return (
             <div className="search">
-                <input defaultValue="Search for"></input>
-                <div className="results" >Search results</div>  {/* todo: replace with StationList  */}
+                <input defaultValue="" onInput={ (e) => { this.searchTextChanged(e) } }></input>
+                <div className="results" >
+                    Search results {/* todo: replace with StationList  */}
+                </div>  
             </div>
         )
     }
