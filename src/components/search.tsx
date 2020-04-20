744006097e6e5569a1e2d9387f838e36d4c28943
bxantus <drx.trash@gmail.com>
2020-04-20 18:01:37 +0200

    First version of station search results (list will be extracted to a separate component)

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -16,26 +16,37 @@ export default class Search extends React.Component<{}, SearchState> {
     async searchTextChanged(e) {
         const query = e.target.value;
         console.log("Search: ", query)
-        
+        this.scheduleSearch(query)
     }
 
     private searchTimer
     async scheduleSearch(query:string) {
-        this.searchTimer = setInterval(async ()=> {
+        this.searchTimer = clearTimeout(this.searchTimer)
+        this.searchTimer = setTimeout(async () => {
             let search = new RadioSearch({name: query})
             await search.search()
             this.setState({
                 search
             })
-        })
+        }, 400)
     }
 
     render() {
+        const radioSearch = this.state.search; 
+        let results:JSX.Element[] = []
+        if (radioSearch) {
+            results = radioSearch.results.map(station => <div>
+                                                 <h3>{station.name}</h3>
+                                                 <div>{station.country}</div>
+                                                 <hr></hr>
+                                               </div> )
+        }
         return (
             <div className="search">
                 <input defaultValue="" onInput={ (e) => { this.searchTextChanged(e) } }></input>
                 <div className="results" >
                     Search results {/* todo: replace with StationList  */}
+                    {results}
                 </div>  
             </div>
         )
