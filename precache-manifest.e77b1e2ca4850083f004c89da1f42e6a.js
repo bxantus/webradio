ca302cb091a54b01e56f797fdc3aa8a973c82d9a
bxantus <drx.trash@gmail.com>
2020-04-24 10:52:14 +0200

    Updates

---
+++ precache-manifest.e77b1e2ca4850083f004c89da1f42e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9910a86c64e98d23402bae2cced07d",
+    "url": "/webradio/index.html"
+  },
+  {
+    "revision": "59ecbfd39f54cbc54608",
+    "url": "/webradio/static/css/main.34db2c05.chunk.css"
+  },
+  {
+    "revision": "0585e2d3a6c0bb056dec",
+    "url": "/webradio/static/js/2.7b0a6b77.chunk.js"
+  },
+  {
+    "revision": "f7fe43cf5d12b7c6064f2a7e415de116",
+    "url": "/webradio/static/js/2.7b0a6b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ecbfd39f54cbc54608",
+    "url": "/webradio/static/js/main.f7511930.chunk.js"
+  },
+  {
+    "revision": "49c6e2a577030bc45eb6",
+    "url": "/webradio/static/js/runtime-main.7106fdf5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/webradio/precache-manifest.e77b1e2ca4850083f004c89da1f42e6a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webradio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
