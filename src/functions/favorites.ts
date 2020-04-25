314ac4c0684253753edcb81b9bbc0927a04ca0f2
bxantus <drx.trash@gmail.com>
2020-04-25 15:22:12 +0200

    Added favorites module

---
+++ src/functions/favorites.ts
@@ -0,0 +1,58 @@
+import { Station } from "./radioSearch";
+
+export type UpdateCallback = () => any
+
+// uses localStorage to keep a list of favorite stations
+class Favorites {
+    private stations:Station[] = []
+    
+    add(station:Station) {
+        if (!this.isFavorite(station)) {
+            this.stations.push(station)
+            this.save()
+        }
+    }
+
+    remove(station:Station) {
+        const idx = this.stations.findIndex(val => val.id == station.id)
+        if (idx >= 0) {
+            this.stations.splice(idx, 1)
+            this.save()
+        }
+    }
+
+    isFavorite(station:Station):boolean {
+        const stat = this.stations.find(val => val.id == station.id)
+        return stat != undefined
+    }
+
+    get list() {
+        return this.stations
+    }
+
+    private save() { // save to local storage
+        localStorage.setItem("stations", JSON.stringify(this.stations))
+        this.changed()
+    }
+
+    load() { // load from local storage
+        let stats = localStorage.getItem("stations")
+        if (stats) {
+            this.stations = JSON.parse(stats)
+            this.changed()
+        }
+    }
+    
+    private updateCbs:UpdateCallback[] = []
+    onUpdated(ucb:UpdateCallback):UpdateCallback {
+        this.updateCbs.push(ucb)
+        return ucb
+    }
+
+    private changed() {
+        for (const cb of this.updateCbs)
+            cb()
+    }
+}
+
+export const favorites = new Favorites()
