4e1e0efb67e9530d5bc085f5eb76b4d42cd4acbe
bxantus <drx.trash@gmail.com>
2020-12-07 16:24:51 +0100

    Use placehorder text for search, also display caret for input - the old blinking separator is commented out, as it interferes with the caret

--- src/webradio.tsx
+++ src/webradio.tsx
@@ -147,13 +147,14 @@ export default class WebradioApp extends React.Component<{}, RadioState> {
                 <div id="top" className="flexible vertical radio">
                     <div className="header flexible horizontal">
                         <img className="logo" src="/webradio/logo.svg"></img>
-                        <span className={`divider ${searchSelected ? "blinking" : "static"}`}></span>
+                        <span className={`divider static`}></span>
                         <span className={`currently-playing ${searchSelected ? "hidden" : "visible"}`}>
                             {radioPlayer.station?.name}
                         </span>
                         <input className={`search flex1 ${searchSelected ? "visible" : "hidden"}`} 
                             ref={this.searchInput}
                             defaultValue={currentSearch.searchText} 
+                            placeholder="Search"
                             onInput={ (e) => { this.searchTextChanged(e) } }>
                         </input>
                     </div>
