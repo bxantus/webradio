9c96b99f670bd7198e35ffaf8ef3255b34dec85a
bxantus <drx.trash@gmail.com>
2022-12-09 16:57:14 +0100

    Working on stationList...

--- src/components/stationList.tsx
+++ src/components/stationList.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Station } from "../functions/radioApi";
+import { Station } from "../functions/radioApi.ts";
 
 interface StationsProps {
     stations?: Station[]
